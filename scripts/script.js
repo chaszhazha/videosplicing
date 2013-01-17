beafd7e9370133bf2f1891fa40b49c2435af96d5
Charles <charles.chuckles1990@gmail.com>
2013-01-17 11:17:40 -0500

    ......

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,20 @@
 
+var player;
+
 function onYouTubePlayerReady(playerId) {
-	var player = document.getElementById("video_player");
-	player.cueVideoById("L6K8Uq88BEQ");
+	player = document.getElementById("video_player");
+	player.cueVideoById("s2XzoA94Zws");
 	player.playVideo();
 }
+
+
+function add_video_button_click() {
+	if(!player)
+		return;
+	player.cueVideoById(document.getElementById('vid').value);
+	player.playVideo();
+}
+
 
 $(document).ready(function() {
     	var range_selector = $("#range_selector");
@@ -13,4 +24,6 @@ $(document).ready(function() {
 	var params = { allowScriptAccess: "always" };
     	var atts = { id: "video_player" };//The id for the inserted element by the API
     	swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=player1", "YTplayerHolder", "480", "295", "9", null, null, params, atts);
+	var $add_video_button = $("#add_video_button");
+	$add_video_button.click = add_video_button_click;
 } );
