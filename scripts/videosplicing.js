8e5b0b8e7bcb289e51f84c6905f78a9a2d037516
Charles <charles.chuckles1990@gmail.com>
2013-01-18 19:37:19 -0500

    ...

--- scripts/videosplicing.js
+++ scripts/videosplicing.js
@@ -44,6 +44,7 @@ function onYouTubePlayerReady(playerId) {
 	player = document.getElementById("video_player");
 	player.cueVideoById("s2XzoA94Zws");
 	player.playVideo();
+	player.style.margin = "0 auto";
 }
 
 
@@ -57,20 +58,29 @@ function add_video_button_click() {
 
 
 (function($){
-	$.fn.videosplicer = function(param) {
-	    	this.html("<div id='video_workspace'>" + 
-				"<div>" + 
+	$.fn.videosplicer = function(opt) {
+		opt = opt || {};
+		var default_opt = {player_height: 295, player_width:480};
+		var option = $.extend({},default_opt, opt);
+	    	this.html(
+				"<div id='vid_input'>" + 
 					"<span>Type video id here:</span><input type='text' id='vid'></input><button id='add_video_button'>Add video</button>" + 
 				"</div>" + 
-                		"<div id='YTplayerHolder'>You need Flash player 8+ and JavaScript enabled to view this video.</div> " + 
+                		"<div id='video_container'><div id='YTplayerHolder'>You need Flash player 8+ and JavaScript enabled to view this video.</div></div> " + 
                 		"<div id='range_selector'></div>" +
-                		"<div id='timeline'></div>" + 
-			"</div>");
+                		"<div id='timeline'></div>");
+		$("#vid").css({width:"200px"});
+		$("head").append("<style>" + 
+				"#video_container{margin:0 auto;  width:" + option.player_width + "px;}" + 
+				"#vid_input{margin:0 auto; width: 500px;}" + 
+				"</style>");
 	    	var params = { allowScriptAccess: "always" };
     	    	var atts = { id: "video_player" };//The id for the inserted element by the API
     	    	swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=player1", "YTplayerHolder", "480", "295", "9", null, null, params, atts);
-		var range_selector = $("#range_selector");
-		range_selector.slider({range: true});
+		var $range_selector = $("#range_selector");
+		console.log($range_selector.parent().width());
+		//$range_selector.css({width: ""});
+		$range_selector.slider({range: true});
 		var $add_video_button = $("#add_video_button");
 		$add_video_button.click(add_video_button_click);
 	
