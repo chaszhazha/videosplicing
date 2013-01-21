91ece301f2dc57078b944857be7646ddfed0f4ea
Charles Zhang <chaszhazha@cs.brown.edu>
2013-01-20 20:10:27 -0500

    ...

--- scripts/videosplicing.js
+++ scripts/videosplicing.js
@@ -23,8 +23,10 @@ function VideoClip(param) {
 function CompositeVideo() { // Composite video class
 	this.links = [];
 	this.videos = [];
+	this.current = 0; // the index of the currently played video
 	this.duration = 0.0;
 	this.position = 0.0;
+	this.isPlaying = false;
 }
 
 CompositeVideo.prototype.AddVideo = function(video_clip)
@@ -36,6 +38,17 @@ CompositeVideo.prototype.AddVideo = function(video_clip)
 	}
 }
 
+CompositeVideo.prototype.UpdateCurrentVideo = function(start, end) 
+{
+	if(end <= start)
+		return;
+	var duration = end - start;
+	var old_duration = this.videos[this.current].end - this.videos[this.current].start;
+	this.duration += (duration - old_duration);
+	this.videos[this.current].end = end;
+	this.videos[this.current].start = start;
+	//TODO: either update the visual for the clips here or somewhere where this function gets called
+}
 
 
 var player;
@@ -51,40 +64,78 @@ function onYouTubePlayerReady(playerId) {
 function add_video_button_click() {
 	if(!player)
 		return;
-	player.cueVideoById(document.getElementById('vid').value);
-	player.playVideo();
-	console.log(player);
+	var $this = $(this);
+	console.log($this.data("videosplicerObj"));
+	xmlhttp=new XMLHttpRequest();
+	var videoid = document.getElementById('vid').value;
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    		{
+			var response = $.parseJSON(xmlhttp.responseText);
+			
+			if(response.items && response.items.length > 0) {
+    				player.cueVideoById(videoid);
+				player.playVideo();
+				console.log(player.getDuration());
+			}
+			else {
+				//TODO: show some pop up containing a message saying that the video id is not a valid one
+			}
+		}
+		else if (xmlhttp.readyState==4 && xmlhttp.status!=200)
+    		{
+    			//TODO: show some pop up containing a message saying that the video id is not a valid one
+		}
+	}
+	xmlhttp.open("GET","https://www.googleapis.com/youtube/v3/videos?id=" + videoid + "&part=id&key=AIzaSyCcjD3FvHlqkmNouICxMnpmkByCI79H-E8",true);
+	xmlhttp.send();
 }
 
 
 (function($){
+	var slider_onslide = function(event, ui) {
+		console.log($(this).data("videosplicerObj"));
+	};
+	
 	$.fn.videosplicer = function(opt) {
 		opt = opt || {};
 		var default_opt = {player_height: 295, player_width:480};
 		var option = $.extend({},default_opt, opt);
 	    	this.html(
 				"<div id='vid_input'>" + 
-					"<span>Type video id here:</span><input type='text' id='vid'></input><button id='add_video_button'>Add video</button>" + 
+					"<span>Type video id here:</span><input type='text' id='vid'></input><button id='splicer_add_video_button'>Add video</button>" + 
 				"</div>" + 
                 		"<div id='video_container'><div id='YTplayerHolder'>You need Flash player 8+ and JavaScript enabled to view this video.</div></div> " + 
-                		"<div id='range_selector'></div>" +
+				"<div id='splicer_time_markers'><span id=''></span></div>" + 
+                		"<div id='splicer_range_selector'></div>" +
+				"<button id='splicer_select_range_button'>Selection range for video clip</button>" + 
                 		"<div id='timeline'></div>");
 		$("#vid").css({width:"200px"});
 		$("head").append("<style>" + 
 				"#video_container{margin:0 auto;  width:" + option.player_width + "px;}" + 
 				"#vid_input{margin:0 auto; width: 500px;}" + 
+				"#splicer_select_range_button{float: right; margin-top: 10px;}" + 
 				"</style>");
 	    	var params = { allowScriptAccess: "always" };
     	    	var atts = { id: "video_player" };//The id for the inserted element by the API
     	    	swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=player1", "YTplayerHolder", "480", "295", "9", null, null, params, atts);
-		var $range_selector = $("#range_selector");
-		console.log($range_selector.parent().width());
+		
+		var $range_selector = $("#splicer_range_selector");
+		//console.log($range_selector.parent().width());
 		//$range_selector.css({width: ""});
-		$range_selector.slider({range: true});
-		var $add_video_button = $("#add_video_button");
+
+		//Initilizing the range slider, set to disabled because there are no video clips included:
+		$range_selector.slider({range: true, slide: slider_onslide});
+		$range_selector.css("margin-top", "10px");
+		//$range_selector.slider("disable");
+		var $add_video_button = $("#splicer_add_video_button");
+		$add_video_button.data("videosplicerObj" , this);
+		$range_selector.data("videosplicerObj" , this);
 		$add_video_button.click(add_video_button_click);
-	
-		this.video_doc = new CompositeVideo();
+		this.data("video_doc" , new CompositeVideo());
+		this.data("range_selector", $range_selector);
+		
+
 		return this;
 	}
 })(jQuery);
